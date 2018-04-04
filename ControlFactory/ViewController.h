fee4558780710b38aafe561565f7e28e7dbe0639
2464556998@qq.com <2464556998@qq.com>
2018-04-04 17:58:14 +0800

    Initial Commit

---
+++ ControlFactory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlFactory
+//
+//  Created by 张涛 on 2018/4/4.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ControlFactory/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ControlFactory
+//
+//  Created by 张涛 on 2018/4/4.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
